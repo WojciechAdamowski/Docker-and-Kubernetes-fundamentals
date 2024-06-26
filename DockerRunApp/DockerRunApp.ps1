c18f8819c7356008ddb0eb4cd34879c7f9c9ff74
Wojciech Adamowski <wojtekadam1@gmail.com>
2024-06-26 11:24:11 +0200

    Change parameters shortcuts to full names

--- DockerRunApp/DockerRunApp.ps1
+++ DockerRunApp/DockerRunApp.ps1
@@ -2,8 +2,8 @@ $repoPath = 'your\repo\path'
 
 # Run app using Docker file 
 Set-Location $repoPath
-docker build --rm --pull -t docker-run-app:v1 -f dockerfile .
-docker run --rm -d --name docker-run-app -p 5000:5000/tcp docker-run-app:v1
+docker build --rm --pull --tag docker-run-app:v1 --file dockerfile .
+docker run --rm --detach --name docker-run-app --publish 5000:5000/tcp docker-run-app:v1
 
 docker images 
 docker ps
@@ -13,7 +13,7 @@ docker stop docker-run-app
 # Run app with volume to store data
 Set-Location $repoPath
 docker volume create docker-run-app-volume
-docker run --rm -d --name docker-run-app -p 5000:5000/tcp -v docker-run-app-volume:/app/data docker-run-app:v1
+docker run --rm --detach --name docker-run-app --publish 5000:5000/tcp -v docker-run-app-volume:/app/data docker-run-app:v1
 
 # Remove all previous created objects
 docker stop docker-run-app
@@ -24,26 +24,26 @@ docker rmi docker-run-app:v1
 Set-Location $repoPath
 docker volume create docker-run-app-volume
 
-docker compose -f docker-compose.yaml -p docker-run-app-compose up -d
+docker compose -f docker-compose.yaml -p docker-run-app-compose up --detach
 docker compose -p docker-run-app-compose down
 
 docker volume rm docker-run-app-volume
 
 # Excersise with volume 
 Set-Location $repoPath
-docker build --rm --pull -t docker-run-app:v1 -f dockerfile .
+docker build --rm --pull --tag docker-run-app:v1 --file dockerfile .
 
-docker run --rm -d --name docker-run-app -p 5000:5000/tcp docker-run-app:v1
+docker run --rm --detach --name docker-run-app --publish 5000:5000/tcp docker-run-app:v1
 docker stop docker-run-app
 
-docker run --rm -d --name docker-run-app -p 5000:5000/tcp docker-run-app:v1
+docker run --rm --detach --name docker-run-app --publish 5000:5000/tcp docker-run-app:v1
 docker stop docker-run-app
 
 docker volume create docker-run-app-volume
-docker run --rm -d --name docker-run-app -p 5000:5000/tcp -v docker-run-app-volume:/app/data docker-run-app:v1
+docker run --rm --detach --name docker-run-app --publish 5000:5000/tcp -v docker-run-app-volume:/app/data docker-run-app:v1
 docker stop docker-run-app
 
-docker run --rm -d --name docker-run-app -p 5000:5000/tcp -v docker-run-app-volume:/app/data docker-run-app:v1
+docker run --rm --detach --name docker-run-app --publish 5000:5000/tcp -v docker-run-app-volume:/app/data docker-run-app:v1
 docker stop docker-run-app
 
 docker volume rm docker-run-app-volume
