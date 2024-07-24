ec7cd2c2ab7fe12dac9039e4d53d5ca0db4363d2
Wojciech Adamowski <wojtekadam1@gmail.com>
2024-07-24 09:26:05 +0200

    Added notes about new pods in powewrshell file

--- KubernetesPods/KubernetesPods.ps1
+++ KubernetesPods/KubernetesPods.ps1
@@ -19,13 +19,24 @@ kubectl describe pod web-app-with-resources
 kubectl apply --filename .\KubernetesPods\Yamls\pod-with-liveness.yaml
 kubectl describe pod web-app-with-liveness
 
+# Create the pod with configured security context
+kubectl apply --filename .\KubernetesPods\Yamls\pod-with-security-context.yaml
+kubectl describe pod web-app-with-security-context
+
+# Create the pod with configured init container
+kubectl apply --filename .\KubernetesPods\Yamls\pod-with-init-container.yaml
+kubectl describe pod web-app-with-init-container
+
 # Check that are every pod exist
 kubectl get pods -o wide 
 kubectl logs web-app-with-liveness
+kubectl logs web-app-with-init-container --container init-my-container
 
 # Delete all created objects for clarity
 kubectl delete --filename .\KubernetesPods\Yamls\pod.yaml
 kubectl delete --filename .\KubernetesPods\Yamls\pod-with-resources.yaml
 kubectl delete --filename .\KubernetesPods\Yamls\pod-with-liveness.yaml
+kubectl delete --filename .\KubernetesPods\Yamls\pod-with-security-context.yaml
+kubectl delete --filename .\KubernetesPods\Yamls\pod-with-init-container.yaml
 
 docker image rm docker-run-app:v1
